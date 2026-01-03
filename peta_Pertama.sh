5062456f2cc86a544982af355c693a82ea823081
kiel117220042 <117220042@student.upnyk.ac.id>
2026-01-03 10:53:18 +0700

    Enhance peta_Pertama.sh with comments
    Added comments to clarify map generation steps.

--- peta_Pertama.sh
+++ peta_Pertama.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 gmt begin   ../output/nano_peta_Pertama png
+    #untuk memberikan kerangka peta
     gmt basemap -R95/141/-11/6 -JM15C -Baf
+    #untuk menunjukan mana yang merupakan daratan dan lautan 
     gmt coast -Glightgray -Slightblue
 gmt end show
